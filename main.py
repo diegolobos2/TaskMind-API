ab6f5acdb1e1153024f02ba107708405d1849dd4
Diego Lobos <diegolobos4@gmail.com>
2026-06-29 00:10:34 -0300

    demo: update health response 3

--- main.py
+++ main.py
@@ -37,6 +37,6 @@ app = FastAPI(
 
 @app.get("/health")
 async def health_check():
-    return {"status": "está bien, nivel 2"}
+    return {"status": "exito absoluto"}
 
 app.include_router(tasks.router, prefix="/api/v1/tasks", tags=["tasks"])
